f6c56bff6baec856ab56e401ddbea0af0cfbc388
Adam Biegert <118407273+AdamaStarkN7@users.noreply.github.com>
2025-06-05 16:39:00 -0600

    Add Ox Lib Dependency Check

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -8,13 +8,13 @@ name 'stark_lockbox'
 
 author 'Adama Stark'
 
-version '2.2.1'
+version '2.2.2'
 
 description 'A Vehicle Lockbox Script for Qbox & QBCore'
 
 shared_script {
     'config.lua',
-    '@ox_lib/init.lua' 
+    '@ox_lib/init.lua'
 }
 
 client_scripts{
--- server/server.lua
+++ server/server.lua
@@ -1,3 +1,5 @@
+if not lib.checkDependency('ox_lib', '3.30.0', true) then return end
+
 if Config.versionCheck == 'true' then
     lib.versionCheck('AdamaStark-N7/stark_lockbox')
 end
