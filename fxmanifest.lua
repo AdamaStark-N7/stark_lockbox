2c6076b1ddbe395f08b48994d7bbd3e68aff53d9
Adam Biegert <118407273+AdamaStarkN7@users.noreply.github.com>
2025-07-13 14:40:46 -0600

    Syntax

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -18,17 +18,17 @@ description 'A Vehicle Lockbox Script for Qbox & QBCore'
 
 shared_script {
     'config.lua',
-    '@ox_lib/init.lua'
+    '@ox_lib/init.lua',
     -- '@lation_ui/init.lua'
 }
 
-client_scripts{
+client_scripts {
     'bridge/client/**.lua',
     'client/*.lua',
     -- '@qbx_core/modules/playerdata.lua' -- For Use With The Qbox Framework, Can Be Commented Out or Removed If Not Used
 }
 
-server_scripts{
+server_scripts {
     'bridge/server/**.lua',
     'server/*.lua'
 }
