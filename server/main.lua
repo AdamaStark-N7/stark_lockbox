a9485f50ed9b404e802cd372791da6c70a8a04f9
Adam Biegert <theadman2092@ymail.com>
2026-05-17 13:25:21 -0600

    Add files via upload
    Version 5.0 Update

--- server/main.lua
+++ server/main.lua
@@ -5,3 +5,20 @@ local Config = require 'shared.config'
 if Config.VersionCheck then
     lib.versionCheck('AdamaStark-N7/stark_lockbox')
 end
+
+Framework = nil
+
+if GetResourceState('es_extended') == 'started' then
+    Framework = 'ESX'
+elseif GetResourceState('qb-core') == 'started' and GetResourceState('qbx_core') ~= 'started' then
+    Framework = 'qb'
+elseif GetResourceState('qbx_core') == 'started' then
+    Framework = 'qbx'
+else
+    Framework = nil
+    lib.print.error(locale('error.framework_warning'))
+end
+
+if Config.Debug then
+    lib.print.info('[stark_lockbox] Framework Detected: ' .. tostring(Framework))
+end
